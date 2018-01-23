209bf087404748949638669402c2779da73ff5a1
awais <owaesmirza@gmail.com>
2018-01-23 22:11:20 +1100

    Created a room JavaScript Room Implementation

--- index.js
+++ index.js
@@ -19,10 +19,16 @@ const tech = io.of('/tech');
 
 
 tech.on('connection', (socket) => {
+
+    socket.on('join', (data) =>{
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user joined ${data.room} room!`);
+    });
+
     console.log('User Connected');
-    socket.on('message', (msg) => {
-        console.log(`message: ${msg}`);
-        tech.emit('message', msg);
+    socket.on('message', (data) => {
+        console.log(`message: ${data.msg}`);
+        tech.in(data.room).emit('message', data.msg);
     });
 
    io.on('disconnect', () =>{
